b3c8d774ccae92cf900aed4e96fdecb7605ed048
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-04-06 17:08:16 +0700

    Add index file

---
+++ src/component/index.tsx
@@ -0,0 +1,3 @@
+export { default as Footer } from './footer/Footer';
+export { default as LandingNavBar } from './landingNavbar/LandingNavbar';
+export { default as LandingBackground } from './landingBackground/LandingBackground';
