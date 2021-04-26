30dd4a8a0e7dfdd148001f62450aec9641fa268d
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-04-27 01:32:40 +0700

    Implement InputModal

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -4,3 +4,4 @@ export { default as StaffNavbar } from './staffNavbar/StaffNavbar';
 export { default as BasicLayout } from './basicLayout/BasicLayout';
 export { default as Container } from './container/Container';
 export { default as ConfirmModal } from './confirmModal/ConfirmModal';
+export { default as InputModal } from './inputModal/InputModal';
