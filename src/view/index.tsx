797c623f33fb31b19d1aef6bd46329b20e50c823
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-04-28 02:32:58 +0700

    Implement subcategory management

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,4 +1,5 @@
 export { default as Home } from './home/Home';
 export { default as SignIn } from './signIn/SignIn';
 export { default as QuestionManagement } from './questionManagement/QuestionManagement';
+export { default as SubcategoryManagement } from './subcategoryManagement/SubcategoryManagement';
 export { default as CategoryManagement } from './categoryManagement/CategoryManagement';
---
+++ src/view/subcategoryManagement/domain/subcategory.tsx
@@ -0,0 +1,6 @@
+export const category = { id: 'category-01', category: 'Admission' };
+export const subcategories = [
+  { id: 'subcategory-01', subcategory: 'Active Recruitment' },
+  { id: 'subcategory-02', subcategory: '2B-KMUTT' },
+  { id: 'subcategory-03', subcategory: 'Petch Prajom' },
+];
