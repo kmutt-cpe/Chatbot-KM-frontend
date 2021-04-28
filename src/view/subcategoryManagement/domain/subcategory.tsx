2a7a28bbef4cf0f467a6d22c8ef590a248de9fbb
Nathaphop <Nathaphop.s@mail.kmutt.ac.th>
2021-04-29 03:26:11 +0700

    Add pagination in table of subcategory and fix bug

--- src/view/subcategoryManagement/domain/subcategory.tsx
+++ src/view/subcategoryManagement/domain/subcategory.tsx
@@ -3,4 +3,7 @@ export const subcategories = [
   { id: 'subcategory-01', subcategory: 'Active Recruitment' },
   { id: 'subcategory-02', subcategory: '2B-KMUTT' },
   { id: 'subcategory-03', subcategory: 'Petch Prajom' },
+  { id: 'subcategory-04', subcategory: 'Subcat4' },
+  { id: 'subcategory-05', subcategory: 'Subcat5' },
+  { id: 'subcategory-06', subcategory: 'Subcat6' },
 ];
